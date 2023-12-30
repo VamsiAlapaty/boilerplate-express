75e09c48365e372bfd6bc936dd572cff29834fc7
Vamsi <saivamsi7996@gmail.com>
2023-12-29 23:46:43 -0500

    addedKeyValue

--- myApp.js
+++ myApp.js
@@ -1,12 +1,13 @@
 let express = require('express');
 let app = express();
 // console.log("Hello World")
-// app.use("/public", express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
 app.get('/json',function(req,res){
+    res.send(__dirname + "/views/index.html")
     res.json(
-        {"message":"Hello json"}
-    )
-})
+        {message:"Hello json"}
+    );
+});
 
 
 
