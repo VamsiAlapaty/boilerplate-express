c02e860c154b91e29358553276422d6c26b24acb
Vamsi <saivamsi7996@gmail.com>
2023-12-29 23:51:24 -0500

    minFix

--- myApp.js
+++ myApp.js
@@ -3,9 +3,8 @@ let app = express();
 // console.log("Hello World")
 app.use("/public", express.static(__dirname + "/public"));
 app.get('/json',function(req,res){
-    res.send(__dirname + "/views/index.html")
     res.json(
-        {message:"Hello json"}
+        { message: "Hello json"}
     );
 });
 
